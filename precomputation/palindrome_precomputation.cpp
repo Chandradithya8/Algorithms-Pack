b754ceb131bfe56418a4a641eb55d20fc53cd465
CHANDRADITHYA K.G <64347511+Chandradithya8@users.noreply.github.com>
2023-02-09 17:49:03 +0530

    Update palindrome_precomputation.cpp

--- precomputation/palindrome_precomputation.cpp
+++ precomputation/palindrome_precomputation.cpp
@@ -1,5 +1,12 @@
-bool isPal[2000][2000] = {};
-for(int i = s.size() - 1; i >= 0; --i) {
-    for(int j = i; j < s.size(); ++j)
-        isPal[i][j] = s[i] == s[j] ? i + 1 >= j || isPal[i + 1][j - 1] : false;
+vector<vector<int>> dp(n, vector<int>(n, 0));
+for(int i=0; i<n; i++) {
+    dp[i][i] = 1;
+}
+
+for(int len=2; len<=n; len++) {
+    for(int i=0; i+len-1<n; i++) {
+        int j = i+len-1;
+        if(i+1 == j) dp[i][j] = (s[i] == s[j]);
+        else dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+    }
 }
