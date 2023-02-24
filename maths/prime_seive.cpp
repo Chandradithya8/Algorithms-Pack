050238adc356df26324c224fe56050fab2349c23
CHANDRADITHYA K.G <64347511+Chandradithya8@users.noreply.github.com>
2023-02-24 20:31:30 +0530

    Update prime_seive.cpp

--- maths/prime_seive.cpp
+++ maths/prime_seive.cpp
@@ -16,10 +16,29 @@ void primeSeive(int size) {
     }
 }
 
+// find the least prime factor of a number in range 1 ... n
+vector<long long> leastPrime;
+void least_primeSeive(int size) {
+    leastPrime.resize(size+1, 0);
+    leastPrime[1] = 1;
+    for(long long i=2; i<=size; i++) {
+        if(leastPrime[i] == 0) {
+        	leastPrime[i] = i;
+            for(long long j=i*i; j<=size; j=(j+i)) {
+                if(leastPrime[j] == 0) leastPrime[j] = i;
+            }
+        }
+    }
+}
+
 int main() {
 	primeSeive(100);
-
 	for(int i=0; i<=90; i++) {
 		if(seive[i]) cout << i << " ";
+	}
+	
+	least_primeSeive(100);
+	for(int i=0; i<=90; i++) {
+		cout << i << " " << leastPrime[i] << endl;
 	}
 }
